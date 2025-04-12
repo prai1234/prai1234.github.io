0fb9f707a9a8bd31f2298202fc0452292d457c72
Prahlad B Rai <catmando849@gmail.com>
2025-04-11 21:07:00 -0400

    Create styleseed.css

---
+++ itis3135/styleseed.css
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    window.plusSlides = function(n) {
+        showSlides(slideIndex += n);
+    }
+
+    window.currentSlide = function(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = $(".mySlides");
+        let dots = $(".dot");
+
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+
+        slides.hide();
+        dots.removeClass("active");
+
+        slides.eq(slideIndex - 1).show();
+        dots.eq(slideIndex - 1).addClass("active");
+    }
+
+    setInterval(function() {
+        plusSlides(1);
+    }, 5000);
+});
